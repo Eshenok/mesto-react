651b85f24800f9cbb62904c8a4c7f1c0d70d6c3a
Alexander Voloshin <voloshinptu@yandex.ru>
2022-10-25 22:44:20 +0300

    html auth v 1.1

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -5,8 +5,8 @@ export default function AuthForm(props) {
 		<div className="">
 			<p className="">{props.title}</p>
 			<form className="">
-				<Input placeholder="Email"/>
-				<Input placeholder="Пароль"/>
+				<Input type="email" placeholder="Email"/>
+				<Input type="password" placeholder="Пароль"/>
 				<button type="button" className="button">{props.buttonTitle}</button>
 				{() => props.caption ? <p className="">props.caption</p> : <></>}
 			</form>
